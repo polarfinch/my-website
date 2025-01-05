8ccf37faef44ce20cbf487ec1a7dc678e457ce51
polarfinch <fer.hor@gmail.com>
2025-01-05 14:14:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -27,10 +27,11 @@ $(document).ready(function () {
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
 
-  $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
+  $('.btn-mobile-menu').click(function () {
+  $('.navigation-wrapper').toggleClass('visible');  // Toggle visibility
+  $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle');  // Switch between icons
+});
+
 
 })
 
