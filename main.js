8e0cd514ea0fade7c8432d32b8dc685dd67c01f0
polarfinch <fer.hor@gmail.com>
2025-01-05 15:43:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
----
-  layout: null
-sitemap:
-exclude: 'yes'
----
-
-  $(document).ready(function () {
-    {% if site.disable_landing_page != true %}
-    $('a.blog-button').click(function (e) {
-      if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-      currentWidth = $('.panel-cover').width()
-      if (currentWidth < 960) {
-        $('.panel-cover').addClass('panel-cover--collapsed')
-        $('.content-wrapper').addClass('animated slideInRight')
-      } else {
-        $('.panel-cover').css('max-width', currentWidth)
-        $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { })
-      }
-    })
-
-    if (window.location.hash && window.location.hash == '#blog') {
-      $('.panel-cover').addClass('panel-cover--collapsed')
+$(document).ready(function () {
+  // Handle blog button clicks (this logic seems unrelated to the burger menu)
+  {% if site.disable_landing_page != true %}
+  $('a.blog-button').click(function (e) {
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    currentWidth = $('.panel-cover').width();
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed');
+      $('.content-wrapper').addClass('animated slideInRight');
+    } else {
+      $('.panel-cover').css('max-width', currentWidth);
+      $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { });
     }
+  });
 
-    if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-    }
-    {% endif %}
+  if (window.location.hash && window.location.hash == '#blog') {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+  }
 
-    $(document).ready(function () {
+  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+  }
+  {% endif %}
+
   // Toggle menu visibility when burger icon is clicked
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown'); // Show/hide the menu
@@ -40,10 +34,4 @@ exclude: 'yes'
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn'); // Show the burger icon again
   });
 });
-
-
-    
-
-  })
-
 
